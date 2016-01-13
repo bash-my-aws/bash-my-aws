58275fe8e9ba4bb85bf40001cc1c723b81e6c5df
Jon Higgs <jhiggs@eml.cc>
2016-01-13 17:36:53 +1100

    Add bash-completion for elb-functions

--- bash_completion.sh
+++ bash_completion.sh
@@ -1,3 +1,11 @@
+_bma_elbs_completion() {
+    local command="$1"
+    local word="$2"
+    local options=$(elbs)
+    COMPREPLY=($(compgen -W "${options}" -- ${word}))
+    return 0
+}
+
 _bma_stacks_completion() {
   local command="$1"
   local word="$2"
@@ -77,4 +85,5 @@ complete -F _bma_stacks_completion stack-tail
 complete -F _bma_stacks_completion stack-template
 complete -F _bma_stacks_completion stack-outputs
 complete -F _bma_stacks_completion stack-diff
+complete -F _bma_elbs_completion elb-instances
 complete -f stack-validate
